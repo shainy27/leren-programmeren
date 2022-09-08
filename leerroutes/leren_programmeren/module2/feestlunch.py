8cc2a082020251f37ccf06be5a89a90322642d4e
shainy27 <99074630@mydavinci.nl>
2022-09-08 10:26:43 +0200

    feestlunch en speelhal opdrachten

---
+++ leerroutes/leren_programmeren/module2/feestlunch.py
@@ -0,0 +1,10 @@
+x = float(0.39)
+y = float(2.78)
+z = float(0.50)
+
+
+
+print(x * 17 + y * 2 - 3 * z )
+feestlunch = x * 17 + y * 2 - 3 * z
+
+print("De feestlunch kost je bij de bakker" , feestlunch , "euro voor de 17 croissantjes en de 2 stokbroden als de 3 kortingsbonnen nog geldig zijn!")
--- leerroutes/leren_programmeren/module2/speelhal.py
+++ leerroutes/leren_programmeren/module2/speelhal.py
@@ -4,3 +4,7 @@ vrgamen = float(0.39)
 gamen = vrgamen * 9
 
 print(speelhal * 4 + gamen * 4)
+dagje_uit = speelhal * 4 + gamen * 4
+
+
+print("Dit geweldige dagje-uit met 4 mensen in de Speelhal met 45 minuten VR kost je maar", speelhal * 4 + gamen * 4 ,"euro")
