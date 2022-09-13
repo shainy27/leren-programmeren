7ccc52671d7f26139b0bdd51430f703e96cf048a
shainy27 <99074630@mydavinci.nl>
2022-09-13 14:33:02 +0200

    kaasflow opdracht

---
+++ leerroutes/leren_programmeren/module2_omgaan_met/tijd.py
@@ -0,0 +1,18 @@
+#vraag uren tijd
+#vraag minuten
+#dan duurt het nog ___ tot de dag voorbij is
+
+totaal_uren= 23
+totaal_minuten= 60
+
+
+tijd_uren = int(input("Hoeveel uur?"))
+tijd_minuten = int(input("Hoeveel minuten?"))
+
+totaaltijd_uur = (totaal_uren - tijd_uren)
+totaaltijd_minuten = (totaal_minuten - tijd_minuten)
+
+
+
+
+print(f'dan duurt het nog {totaaltijd_uur}:{totaaltijd_minuten}')
