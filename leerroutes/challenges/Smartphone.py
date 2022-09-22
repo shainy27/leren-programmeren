89eecd294218e6cdcd5f94caa5772206168ff4c1
shainy27 <99074630@mydavinci.nl>
2022-09-22 12:12:36 +0200

    smartphone opdrachten

--- leerroutes/challenges/Smartphone.py
+++ leerroutes/challenges/Smartphone.py
@@ -3,32 +3,54 @@
 from operator import ipow
 
 
-iphone = float(input("Hoeveel kost de Iphone 13?"))
-samsung = float(input("Hoeveel kost de Samsung Galaxy S22?"))
+iphone = round(float(input("Hoeveel kost de Iphone 13?")))
+samsung = round(float(input("Hoeveel kost de Samsung Galaxy S22?")))
+verschil = round(float(iphone - samsung))
 
 
 
+#als de iphone en samsung hoger dan 900 zijn print hij de voorkeur en eindigt het programma
+if iphone and samsung >900:    
+    print(f"De Samsung Galaxy S22 kost meer dan 900 Euro, de telefoon kost: {samsung} Euro")
+    print(f"De Iphone 13 kost meer dan 900 Euro, de telefoon kost: {iphone} Euro")
+    print("Het advies is dus geen telefoon te kopen, ze zijn te duur.")
+    exit()
 
+
+#als het verschil minder dan 50 is en de samsung duurder is print het programma de voorkeur en eindigt hij.
+if verschil <50 and samsung > iphone:
+    print(f"De Samsung galaxy S22 is het duurst, de telefoon kost: {samsung} Euro")
+    print(f"De Iphone 13 is het goedkoopst, de telefoon kost: {iphone} Euro")
+    print(f"Naar voorkeur is het advies om de iphone te kopen. Het verschil is namelijk maar {verschil} Euro")
+    exit()
+
+#als het verschil minder dan 50 is en de iphone duurder dan samsung is print het programma de voorkeur en eindigt hij
+if verschil <50 and iphone > samsung:
+    print(f"De Iphone 13 is het duurst, de telefoon kost: {iphone} Euro")
+    print(f"De Samsung Galaxy S22 is het goedkoopst, de telefoon kost: {samsung} Euro")
+    print(f"Naar voorkeur is het advies om de iphone te kopen. Het verschil is namelijk maar {verschil} Euro")
+    exit()
+
+#als de iphone duurder is dan de samsung print de programma de voorkeur en eindigt hij
 if iphone > samsung:
     print(f"De Iphone 13 is het duurst, de telefoon kost: {iphone} Euro")
     print(f"De Samsung is het goedkoopst, de telefoon kost: {samsung} Euro")
-    if iphone and samsung >900:
-        print("Het advies is dus geen telefoon te kopen, ze zijn te duur.")
-    else:
-         print(f"Het advies is dus de Samsung te kopen. Deze is namelijk {iphone - samsung} Euro goedkoper dan de Iphone 13.")
+    print(f"Het advies is dus de Samsung te kopen. Deze is namelijk {iphone - samsung} Euro goedkoper dan de Iphone 13.")
+    exit()
 
+#als de samsung duurder is dan de iphone print de programma de voorkeur en eindigt hij
 if samsung > iphone:
     print(f"De Samsung galaxy S22 is het duurst, de telefoon kost: {samsung} Euro")
     print(f"De Iphone 13 is het goedkoopst, de telefoon kost: {iphone} Euro")
-    if iphone and samsung >900:
-        print("Het advies is dus geen telefoon te kopen, ze zijn te duur.")
-    else:
-         print(f"Het advies is dus de Iphone te kopen. Deze is namelijk {samsung - iphone} Euro goedkoper dan de Samsung Galaxy S22.")
+    print(f"Het advies is dus de Iphone te kopen. Deze is namelijk {samsung - iphone} Euro goedkoper dan de Samsung Galaxy S22.")
+    exit()
 
+#als de samsung en de iphone even duur zijn print de programma de voorkeur en eindight hij
 elif samsung == iphone:
     print(f"De telefoons zijn even duur.")
     if iphone and samsung >900:
-        print("Het advies is dus geen telefoon te kopen, ze zijn te duur.")
+        print("Het advies is geen telefoon te kopen, ze zijn te duur.")
     else:
          print(f"Het advies is om bij voorkeur de Iphone 13 te kopen.")
+
 
--- leerroutes/challenges/smartphone_uitbreiding.py
+++ leerroutes/challenges/smartphone_uitbreiding.py
@@ -1,52 +1,56 @@
 # prijs uitrekenen
 
 from operator import ipow
-from tkinter import END
+
+
+iphone = round(float(input("Hoeveel kost de Iphone 13?")))
+samsung = round(float(input("Hoeveel kost de Samsung Galaxy S22?")))
+verschil = round(float(iphone - samsung))
 
 
 
+#als de iphone en samsung hoger dan 900 zijn print hij de voorkeur en eindigt het programma
+if iphone and samsung >900:    
+    print(f"De Samsung Galaxy S22 kost meer dan 900 Euro, de telefoon kost: {samsung} Euro")
+    print(f"De Iphone 13 kost meer dan 900 Euro, de telefoon kost: {iphone} Euro")
+    print("Het advies is dus geen telefoon te kopen, ze zijn te duur.")
+    exit()
 
 
-iphone = float(input("Hoeveel kost de Iphone 13?:"))
-samsung = float(input("Hoeveel kost de Samsung Galaxy S22?:"))
-asus = float(input("Hoeveel kost de Asus Zenfone 9?:"))
+#als het verschil minder dan 50 is en de samsung duurder is print het programma de voorkeur en eindigt hij.
+if verschil <50 and samsung > iphone:
+    print(f"De Samsung galaxy S22 is het duurst, de telefoon kost: {samsung} Euro")
+    print(f"De Iphone 13 is het goedkoopst, de telefoon kost: {iphone} Euro")
+    print(f"Naar voorkeur is het advies om de iphone te kopen. Het verschil is namelijk maar {verschil} Euro")
+    exit()
 
+#als het verschil minder dan 50 is en de iphone duurder dan samsung is print het programma de voorkeur en eindigt hij
+if verschil <50 and iphone > samsung:
+    print(f"De Iphone 13 is het duurst, de telefoon kost: {iphone} Euro")
+    print(f"De Samsung Galaxy S22 is het goedkoopst, de telefoon kost: {samsung} Euro")
+    print(f"Naar voorkeur is het advies om de iphone te kopen. Het verschil is namelijk maar {verschil} Euro")
+    exit()
 
+#als de iphone duurder is dan de samsung print de programma de voorkeur en eindigt hij
+if iphone > samsung:
+    print(f"De Iphone 13 is het duurst, de telefoon kost: {iphone} Euro")
+    print(f"De Samsung is het goedkoopst, de telefoon kost: {samsung} Euro")
+    print(f"Het advies is dus de Samsung te kopen. Deze is namelijk {iphone - samsung} Euro goedkoper dan de Iphone 13.")
+    exit()
 
-while True:
+#als de samsung duurder is dan de iphone print de programma de voorkeur en eindigt hij
+if samsung > iphone:
+    print(f"De Samsung galaxy S22 is het duurst, de telefoon kost: {samsung} Euro")
+    print(f"De Iphone 13 is het goedkoopst, de telefoon kost: {iphone} Euro")
+    print(f"Het advies is dus de Iphone te kopen. Deze is namelijk {samsung - iphone} Euro goedkoper dan de Samsung Galaxy S22.")
+    exit()
 
-    if iphone > samsung and iphone > asus:
-        print(f"De Iphone 13 is het duurst, de telefoon kost: {iphone} Euro")
-        if iphone and samsung and asus >900:
-            print("Het advies is dus geen telefoon te kopen, ze zijn te duur.")
-            exit()
-        if asus > samsung:
-            print("samsung goedkoopst")
-            print("asus ertussenin")
-            print("Het advies is dus de samsung te kopen deze is namelijk")
-        elif samsung > asus:
-            print("asus goedkoopst")
-            print("samsung ertussenin")
-            print("Het advies is dus de asus te kopen deze is namelijk")
+#als de samsung en de iphone even duur zijn print de programma de voorkeur en eindight hij
+elif samsung == iphone:
+    print(f"De telefoons zijn even duur.")
+    if iphone and samsung >900:
+        print("Het advies is geen telefoon te kopen, ze zijn te duur.")
+    else:
+         print(f"Het advies is om bij voorkeur de Iphone 13 te kopen.")
 
-        if iphone and samsung and asus >900:
-            print("Het advies is dus geen telefoon te kopen, ze zijn te duur.")
-        else: 
-            print(f"Het advies is dus de Samsung te kopen. Deze is namelijk {iphone - samsung} Euro goedkoper dan de Iphone 13. En {asus - samsung} Euro goedkoper dan de Asus")
-
-    if samsung > iphone and samsung > asus:
-        print(f"De Samsung galaxy S22 is het duurst, de telefoon kost: {samsung} Euro")
-        print(f"De Iphone 13 is het goedkoopst, de telefoon kost: {iphone} Euro")
-        if iphone and samsung >900:
-            print("Het advies is dus geen telefoon te kopen, ze zijn te duur.")
-        else:
-            print(f"Het advies is dus de Iphone te kopen. Deze is namelijk {samsung - iphone} Euro goedkoper dan de Samsung Galaxy S22.")
-
-    elif samsung == iphone == asus:
-        print(f"De telefoons zijn even duur.")
-        if iphone and samsung and asus >900:
-            print("Het advies is dus geen telefoon te kopen, ze zijn te duur.")
-        else:
-            print(f"Het advies is om bij voorkeur de Iphone 13 te kopen.")
-            break
 
