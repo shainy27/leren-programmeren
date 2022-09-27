851bd7dbc5d820041bd161047b5a04c876315965
shainy27 <99074630@mydavinci.nl>
2022-09-27 13:28:25 +0200

    game

--- leerroutes/challenges/smartphone_uitbreiding2.py
+++ leerroutes/challenges/smartphone_uitbreiding2.py
@@ -9,52 +9,79 @@ from tkinter.messagebox import askquestion
 
 iphone = round(float(input("Hoeveel kost de Iphone 13?")))
 samsung = round(float(input("Hoeveel kost de Samsung Galaxy S22?")))
-asus = round(float(input("Hoeveel kost de Asus?:")))
-verschil = round(float(iphone - samsung))
+asus = round(float(input("Hoeveel kost de Asus Zenfone 9?:")))
+
+verschil= iphone - samsung #if iphone niet duurder dan 50 euro van samsung
+verschil1= samsung - asus #if zenfone 100 euro goedkoper dan samsung
+verschil2= iphone - asus #if zenfone 100 euro goedkoper dan Iphone
 
 
+maxgetal= max(samsung , iphone , asus)
+mingetal= min(samsung , iphone , asus)
 
-#als de iphone en samsung hoger dan 900 zijn print hij de voorkeur en eindigt het programma
-if iphone and samsung >900:    
-    print(f"De Samsung Galaxy S22 kost meer dan 900 Euro, de telefoon kost: {samsung} Euro")
-    print(f"De Iphone 13 kost meer dan 900 Euro, de telefoon kost: {iphone} Euro")
-    print("Het advies is dus geen telefoon te kopen, ze zijn te duur.")
-    exit()
+def middle(samsung, iphone, zenFone) : 
+    return min(max(samsung,iphone),max(iphone,asus),max(samsung,asus))
 
 
-#als het verschil minder dan 50 is en de samsung duurder is print het programma de voorkeur en eindigt hij.
-if verschil <50 and samsung > iphone:
-    print(f"De Samsung galaxy S22 is het duurst, de telefoon kost: {samsung} Euro")
-    print(f"De Iphone 13 is het goedkoopst, de telefoon kost: {iphone} Euro")
-    print(f"Naar voorkeur is het advies om de iphone te kopen. Het verschil is namelijk maar {verschil} Euro")
-    exit()
+if iphone == maxgetal:
+     print (f"de iphone is dus het duurste, de telefoon kost {iphone} euro")
+elif samsung == maxgetal:
+     print (f'de samsung is dus het duurste, de telefoon kost {samsung} euro ')
+else:
+     print (f' deZenFone is dus het duurste, de telefoon kost {asus} euro')
 
-#als het verschil minder dan 50 is en de iphone duurder dan samsung is print het programma de voorkeur en eindigt hij
-if verschil <50 and iphone > samsung:
-    print(f"De Iphone 13 is het duurst, de telefoon kost: {iphone} Euro")
-    print(f"De Samsung Galaxy S22 is het goedkoopst, de telefoon kost: {samsung} Euro")
-    print(f"Naar voorkeur is het advies om de iphone te kopen. Het verschil is namelijk maar {verschil} Euro")
-    exit()
+# goedkoopste  
+()   
+if iphone == mingetal:
+     print (f"de iphone is dus het goedkoopste, de telefoon kost {iphone} euro")
+     
+elif samsung == mingetal:
+     print (f'de samsung is dus het goedkoopste, de telefoon kost {samsung} euro ')
+else:
+     print (f'de ZenFone is dus het goedkoopste, de telefoon kost {asus} euro')
 
-#als de iphone duurder is dan de samsung print de programma de voorkeur en eindigt hij
-if iphone > samsung:
-    print(f"De Iphone 13 is het duurst, de telefoon kost: {iphone} Euro")
-    print(f"De Samsung is het goedkoopst, de telefoon kost: {samsung} Euro")
-    print(f"Het advies is dus de Samsung te kopen. Deze is namelijk {iphone - samsung} Euro goedkoper dan de Iphone 13.")
-    exit()
-
-#als de samsung duurder is dan de iphone print de programma de voorkeur en eindigt hij
-if samsung > iphone:
-    print(f"De Samsung galaxy S22 is het duurst, de telefoon kost: {samsung} Euro")
-    print(f"De Iphone 13 is het goedkoopst, de telefoon kost: {iphone} Euro")
-    print(f"Het advies is dus de Iphone te kopen. Deze is namelijk {samsung - iphone} Euro goedkoper dan de Samsung Galaxy S22.")
-    exit()
-
-#als de samsung en de iphone even duur zijn print de programma de voorkeur en eindight hij
+#als het gelijk is
+()
+if iphone == samsung and iphone == asus and samsung == asus:
+     gelijk= print ('De prijzen zijn gelijk')
 elif samsung == iphone:
-    print(f"De telefoons zijn even duur.")
-    if iphone and samsung >900:
-        print("Het advies is geen telefoon te kopen, ze zijn te duur.")
-    else:
-         print(f"Het advies is om bij voorkeur de Iphone 13 te kopen.")
+     gelijk= print ('De samsung en iphone zijn gelijk')
+elif samsung == asus:
+     gelijk= print ('De samsung en zenFone zijn gelijk')
+elif iphone == asus:
+     gelijk= print ('De ZenFone en Iphone zijn gelijk')
+
+#tussenin
+()
+if iphone == middle(samsung, iphone, asus):
+     print (f"de iphone zit er tussenin met {iphone} euro")
+elif samsung == middle(samsung, iphone, asus):
+     print (f'de samsung zit er tussenin met {samsung} euro ')
+else:
+     print (f' de ZenFone zit er tussenin met {asus} euro')
+
+#het advies
+if samsung > 900 or iphone > 900 or asus > 900:
+     print ("het advies is dus geen telefoon te kopen, ze zijn te duur!")
+elif verschil1 >= 100 and verschil2 >= 100:
+     print (f'het advies is dus om ZenFone te kopen. Deze is namelijk {verschil1} goedkoper dan samsung en {verschil2} goedkoper dan Iphone ')
+elif verschil >= 50:
+     print (f'"het advies is dus de Iphone te kopen. Deze is namelijk {verschil} goedkoper dan de samsung en {verschil2} goedkoper dan de Zenphone!"')
+elif iphone == mingetal:
+     print (f'"het advies is dus de iphone te kopen. Deze is namelijk {verschil} goedkoper dan de samsung en {verschil2} goedkoper dan de zenPhone"') 
+elif samsung == mingetal:     
+     print (f'"het advies is dus de samsung te kopen. Deze is namelijk {verschil} goedkoper dan de Iphone en {verschil1} goedkoper dan de zenPhone"') 
+
+
+
+
+
+
+
+
+
+
+         
+
+
 
--- leerroutes/leren_programmeren/module2_omgaan_met/adventure_game.py
+++ leerroutes/leren_programmeren/module2_omgaan_met/adventure_game.py
@@ -1,4 +1,5 @@
 #Adventure game
+from ast import If
 from operator import truediv
 from queue import Empty
 
@@ -12,23 +13,46 @@ def main():
     from tkinter import END, N
     from threading import Event
 
-
+    #hier word je naam gevraagd
     name = input("What is your name?")
-
-    print(f'''Welcome to the Jungle {name}! In this game you will make your way through the''')
+    #uitleg voor de game
+    print(f"Welcome to the Jungle {name}! In this game you will make your way through the")
     print("dangerous jungle in an attempt to find the hidden treasure.")
+    Event().wait(3)
 
 
+    print('''        
+                           .=""_;=.
+                       ,-"_,=""     `"=.
+                       "=._o`"-._        `"=.
+                           `"=._o`"=._      _`"=._                    
+                                :=._o "=._."_.-="'"=.
+                         __.--" , ; `"=._o." ,-"""-._ ".  
+                      _._"  ,. .` ` `` ,  `"-._"-._   ".'
+                      |o`"=._` , "` `; .". ,  "-._"-._; ;           
+                      | ;`-.o`"=._; ." ` '`."\` . "-._ /
+                      |o;    `"-.o`"=._``  '` " ,__.--o;
+                      | ;     (#) `-.o `"=.`_.--"_o.-; ;
+                      |o;._    "      `".o|o_.--"    ;o;
+                       "=._o--._        ; | ;        ; ;
+                            "=._o--._   ;o|o;     _._;o;
+                                 "=._o._; | ;_.--"o.--"
+                                      "=.o|o_.--""
+    ''')
+
+
+    print("You have a map to the treasure but it is not complete, so you'll have to search around until you find the treasure yourself.")
+
+    #hier word er gevraagd of de user wil spelen
     while True:
         play = str(input("Are you ready to play? Y/N"))
 
-        if play in ( "N" ,"n", "no") :
+        if play in ( "N" ,"n", "no" , "No") :
           print('''Till next time!
           ''')
           exit()
         
-
-        elif play in ("Y", "y" ,"yes"):
+        elif play in ("Y", "y" ,"yes","Yes"):
           print('''
           Let's start the game!
           ''')
@@ -40,8 +64,8 @@ def main():
             continue
         
 
-    Event().wait(3)
-
+    Event().wait(2)
+    #meer uitleg voor de game
     print('''
     To win the game you need to explore the jungle and find the treasure.
     You also need to make sure you dont die.
@@ -55,11 +79,330 @@ def main():
      ''')
 
     Event().wait(3)
+    #je krijgt de keuze om water te drinken
+    while True:
 
-    print('''
-    You're walking through the jungle and find a blue backpack lying on the floor. it might come in handy, you decide to take it with you.
-    This is now your inventory
+      answer = input("You're walking through The jungle when suddenly you find a small lake with muddy water. Take a sip of water? Y/N:")
+      if answer in ("Y", "y" , "yes", "Yes"):
+        print("You go take a sip of the muddy water. The water is poisonous. You die a painful death. Game Over")
+        exit()
+
+      elif answer in ("N", "n", "no", "No"):
+        print("You continue walking")
+        break
+
+      else:
+        print("please enter a valid choice!")
+        continue
+
+    Event().wait(1)  
+    #je komt een slang tegen, je krijgt de keuze om te vechten of te rennen
+    while True:  
+      print(r'''\   
+                     ---_ ......._-_--.
+                    (|\ /      / /| \  \
+                    /  /     .'  -=-'   `.
+                  /  /    .'             )
+                _/  /   .'        _.)   /
+                / o   o        _.-' /  .'
+                \          _.-'    / .'*|
+                \______.-'//    .'.' \*|
+                  \|  \ | //   .'.' _ |*|
+                  `   \|//  .'.'_ _ _|*|
+                    .  .// .'.' | _ _ \*|
+                    \`-|\_/ /    \ _ _ \*\
+                    `/'\__/      \ _ _ \*\
+                    /^|            \ _ _ \*
+                  '  `             \ _ _ \      
+                                    \_
+           ''')
+
+      snake = str(input("As you walk around a big tree you come face to face with a snake! Run or fight the snake? Run/Fight:"))
+      if snake in ("run", "r", "Run", "RUN"):
+        print("You run away and escape the snake!")
+        break
+      
+      if snake in ("Fight", "f", "fight", "FIGHT"):
+        print("You try to fight the snake with your bare hands and get bitten. You die of the poison. Game Over!")
+        exit()
+
+      else:
+        print("Please enter a valid choice!")
+        continue
+
+    #je vind een verlaten huisje met een deel van de map erin, als je naar binnen gaat kan je de game sneller winnen
+    while True:
+      print(r'''\
+                                       ____
+                   ____________________|  |_____
+                  /______________________________\ 
+                 /________________________________\  
+                   ||___|___||||||||||||___|__|||     
+                   ||___|___||||||||||||___|__|||        
+                   ||||||||||||||||||||||||||||||
+
+      ''')
+      
+      huisje = str(input("You continue on your path to find the treasure. You see an abandoned wooden house. Do you want to explore it? Y/N"))
+      if huisje in ("Yes", "y" ,"YES", "yes", "Y"):
+        print("You enter the house and find another piece of the map. Now you can get there faster!")
+        print("You take the map with you and head back outside")
+
+        #je krijgt de keuze om bananen te gaan halen
+        fruit = str(input("when you're outside you see some bananas hanging on a tree. Do you want to climb the tree and grab the bananas? Y/N"))
+        if fruit in ("Yes", "y" "YES", "yes", "Y"):
+         print("You climb the tree to grab the fruit when you see a group of monkeys in front of you. They fight you for the bananas. You end up falling to your death. Game Over!")
+         exit()
+        #je krijgt een raadsel dat je op moet lossen, je krijgt 3 kansen
+        if fruit in ("N", "n", "no", "No"):
+          print("You continue on your way to the treasure and walk into a dark cave. There's a text on the wall:")
+          print("Before Mount Everest was discovered, what was the highest mountain in the world?")
+          riddle = str(input("Solve te riddle to win the game:"))
+          if riddle in ("mount everest", "Mount Everest", "mount Everest", "Mount everest"):
+            print("Congratulations you found the treasure and won the game!")
+            print('''        
+                           .=""_;=.
+                       ,-"_,=""     `"=.
+                       "=._o`"-._        `"=.
+                           `"=._o`"=._      _`"=._                    
+                                :=._o "=._."_.-="'"=.
+                         __.--" , ; `"=._o." ,-"""-._ ".  
+                      _._"  ,. .` ` `` ,  `"-._"-._   ".'
+                      |o`"=._` , "` `; .". ,  "-._"-._; ;           
+                      | ;`-.o`"=._; ." ` '`."\` . "-._ /
+                      |o;    `"-.o`"=._``  '` " ,__.--o;
+                      | ;     (#) `-.o `"=.`_.--"_o.-; ;
+                      |o;._    "      `".o|o_.--"    ;o;
+                       "=._o--._        ; | ;        ; ;
+                            "=._o--._   ;o|o;     _._;o;
+                                 "=._o._; | ;_.--"o.--"
+                                      "=.o|o_.--""
     ''')
+            exit()
+          
+          else:
+            print("That isn't right. 2 more tries left!")
+            print("Before Mount Everest was discovered, what was the highest mountain in the world?")
+            riddle = str(input("Solve te riddle to win the game:"))
+            if riddle in ("mount everest", "Mount Everest", "mount Everest", "Mount everest"):
+              print("Congratulations you found the treasure and won the game!")
+              print('''        
+                           .=""_;=.
+                       ,-"_,=""     `"=.
+                       "=._o`"-._        `"=.
+                           `"=._o`"=._      _`"=._                    
+                                :=._o "=._."_.-="'"=.
+                         __.--" , ; `"=._o." ,-"""-._ ".  
+                      _._"  ,. .` ` `` ,  `"-._"-._   ".'
+                      |o`"=._` , "` `; .". ,  "-._"-._; ;           
+                      | ;`-.o`"=._; ." ` '`."\` . "-._ /
+                      |o;    `"-.o`"=._``  '` " ,__.--o;
+                      | ;     (#) `-.o `"=.`_.--"_o.-; ;
+                      |o;._    "      `".o|o_.--"    ;o;
+                       "=._o--._        ; | ;        ; ;
+                            "=._o--._   ;o|o;     _._;o;
+                                 "=._o._; | ;_.--"o.--"
+                                      "=.o|o_.--""
+    ''')
+              exit()
+
+            else:
+              print("That isn't right. 1 more try left!")
+
+
+              print("Before Mount Everest was discovered, what was the highest mountain in the world?")
+            riddle = str(input("Solve te riddle to win the game:"))
+            if riddle in ("mount everest", "Mount Everest", "mount Everest", "Mount everest"):
+              print("Congratulations you found the treasure and won the game!")
+              print('''        
+                           .=""_;=.
+                       ,-"_,=""     `"=.
+                       "=._o`"-._        `"=.
+                           `"=._o`"=._      _`"=._                    
+                                :=._o "=._."_.-="'"=.
+                         __.--" , ; `"=._o." ,-"""-._ ".  
+                      _._"  ,. .` ` `` ,  `"-._"-._   ".'
+                      |o`"=._` , "` `; .". ,  "-._"-._; ;           
+                      | ;`-.o`"=._; ." ` '`."\` . "-._ /
+                      |o;    `"-.o`"=._``  '` " ,__.--o;
+                      | ;     (#) `-.o `"=.`_.--"_o.-; ;
+                      |o;._    "      `".o|o_.--"    ;o;
+                       "=._o--._        ; | ;        ; ;
+                            "=._o--._   ;o|o;     _._;o;
+                                 "=._o._; | ;_.--"o.--"
+                                      "=.o|o_.--""
+    ''')
+              exit()
+
+            else:
+              print("That isn't right. No more tries left. Game Over!")
+              exit()
+            
+       #als je het huisje beslist niet in te gaan kom je een leeuw tegen     
+      if huisje in ("N", "n", "no", "No"):
+        print("You walk past the house.")
+        Event().wait(2)
+        print(r'''\       
+                                 .-.       .-.
+                                (   \_.-._/   )
+                                 \           /
+                                 | __     __ |
+                                 | \O\   /O/ |
+                                  \  "   "  /
+                                  /\_`-v-'_/\
+                                 /  \._|_./  \
+                                |    \___/    |
+                                |             |
+                                |             |
+                                |   |     |   |
+                      .ww.     _|   |     |   |_
+                    .\WWW=    / |   |     |   | \
+                    \WWW=    |  |   |     |   |  |
+                    \WW=     |  |   |     |   |  |
+                    ( (      |  |   \     /   |  |
+                    \ \___.-'\  \   \   /   /  /
+                      `-.__.-(...(...)---(...)...)
+        ''')
+        
+        lion = str(input("You continue on your path when you come face to face with a lion. fight or run? Fight/Run"))
+        if lion in ("Fight", "fight",):
+          print("The tiger charges towards you. You grab a rock on the floor and manage to hurt him. You manage to escape and continue on your way")
+          
+
+          #je krijgt de keuze om bananen te halen
+          fruit = str(input("You see some bananas hanging on a tree. Do you want to climb the tree and grab the bananas? Y/N"))
+          if fruit in ("Yes", "y" ,"YES", "yes", "Y"):
+            print("You climb the tree to grab the fruit when you see a group of monkeys in front of you. They fight you for the bananas. You end up falling to your death. Game Over!")
+            exit()
+            s
+           #je krijgt hier een raadsel, je hebt 3 kansen
+          if fruit in ("N", "n", "no", "No"):
+            print("You continue on your way to the treasure and walk into a dark cave. There's a text on the wall:")
+            print("Before Mount Everest was discovered, what was the highest mountain in the world?")
+            riddle = str(input("Solve te riddle to win the game:"))
+            if riddle in ("mount everest", "Mount Everest", "mount Everest", "Mount everest"):
+              print("Congratulations you found the treasure and won the game!")
+              print('''        
+                           .=""_;=.
+                       ,-"_,=""     `"=.
+                       "=._o`"-._        `"=.
+                           `"=._o`"=._      _`"=._                    
+                                :=._o "=._."_.-="'"=.
+                         __.--" , ; `"=._o." ,-"""-._ ".  
+                      _._"  ,. .` ` `` ,  `"-._"-._   ".'
+                      |o`"=._` , "` `; .". ,  "-._"-._; ;           
+                      | ;`-.o`"=._; ." ` '`."\` . "-._ /
+                      |o;    `"-.o`"=._``  '` " ,__.--o;
+                      | ;     (#) `-.o `"=.`_.--"_o.-; ;
+                      |o;._    "      `".o|o_.--"    ;o;
+                       "=._o--._        ; | ;        ; ;
+                            "=._o--._   ;o|o;     _._;o;
+                                 "=._o._; | ;_.--"o.--"
+                                      "=.o|o_.--""
+    ''')
+              exit()
+
+            else:
+              print("That isn't right. 2 more tries left!")
+              print("Before Mount Everest was discovered, what was the highest mountain in the world?")
+              riddle = str(input("Solve te riddle to win the game:"))
+              if riddle in ("mount everest", "Mount Everest", "mount Everest", "Mount everest"):
+                print("Congratulations you found the treasure and won the game!")
+                print('''        
+                           .=""_;=.
+                       ,-"_,=""     `"=.
+                       "=._o`"-._        `"=.
+                           `"=._o`"=._      _`"=._                    
+                                :=._o "=._."_.-="'"=.
+                         __.--" , ; `"=._o." ,-"""-._ ".  
+                      _._"  ,. .` ` `` ,  `"-._"-._   ".'
+                      |o`"=._` , "` `; .". ,  "-._"-._; ;           
+                      | ;`-.o`"=._; ." ` '`."\` . "-._ /
+                      |o;    `"-.o`"=._``  '` " ,__.--o;
+                      | ;     (#) `-.o `"=.`_.--"_o.-; ;
+                      |o;._    "      `".o|o_.--"    ;o;
+                       "=._o--._        ; | ;        ; ;
+                            "=._o--._   ;o|o;     _._;o;
+                                 "=._o._; | ;_.--"o.--"
+                                      "=.o|o_.--""
+    ''')
+                exit()
+
+              else:
+                 print("That isn't right. 1 more try left!")
+                 print("Before Mount Everest was discovered, what was the highest mountain in the world?")
+                 riddle = str(input("Solve te riddle to win the game:"))
+                 if riddle in ("mount everest", "Mount Everest", "mount Everest", "Mount everest"):
+                  print("Congratulations you found the treasure and won the game!")
+                  print('''        
+                           .=""_;=.
+                       ,-"_,=""     `"=.
+                       "=._o`"-._        `"=.
+                           `"=._o`"=._      _`"=._                    
+                                :=._o "=._."_.-="'"=.
+                         __.--" , ; `"=._o." ,-"""-._ ".  
+                      _._"  ,. .` ` `` ,  `"-._"-._   ".'
+                      |o`"=._` , "` `; .". ,  "-._"-._; ;           
+                      | ;`-.o`"=._; ." ` '`."\` . "-._ /
+                      |o;    `"-.o`"=._``  '` " ,__.--o;
+                      | ;     (#) `-.o `"=.`_.--"_o.-; ;
+                      |o;._    "      `".o|o_.--"    ;o;
+                       "=._o--._        ; | ;        ; ;
+                            "=._o--._   ;o|o;     _._;o;
+                                 "=._o._; | ;_.--"o.--"
+                                      "=.o|o_.--""
+    ''')
+                  exit()
+
+                 else:
+                  print("That isn't right. No more tries left. Game Over!")
+                  exit()
+
+
+          
+        #als je met de leeuw probeert te vechten ben je game over
+        if lion in ("Run", "run"):
+          print("You try to run away. The lion is too fast and catches up with you. Game Over")
+          break
+
+      
+      else:
+        print("Please enter a valid answer")
+
+
+
+
+
+
+     
+        
+
+
+
+    
+
+
+
+
+
+
+      
+        
+
+    
+
+
+
+
+    
+
+
+  
+
+    
+
+   
+
     
 
 
