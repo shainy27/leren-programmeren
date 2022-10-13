d72a550416459d4af32ad0c84385e74ea3df01f5
shainy27 <99074630@mydavinci.nl>
2022-10-13 09:49:23 +0200

    zwembad stap 1

---
+++ leerroutes/challenges/zwembad1.py
@@ -0,0 +1,18 @@
+# hoeveel afstand groote zwembad? dat zijn voorrijkosten
+# hoeveel m uitgraven? 
+# hoeveel m3 afvoer?
+
+lengte = float(input("Hoe lang"))
+breedte = float(input("Hoe breed"))
+diepte = float(input("Hoe diep"))
+
+
+
+
+zwembad = lengte * breedte
+
+print(zwembad)
+
+# totaalzwembad = 32,50 * zwembad
+
+# print(f"{totaalzwembad} euro" )
