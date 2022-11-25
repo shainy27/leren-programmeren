dafba0d6ec30ae739deded6fcf80410fd18ebe8d
shainy27 <99074630@mydavinci.nl>
2022-11-25 09:48:00 +0100

    raadeens

--- leerroutes/leren_programmeren/module3_keer_op_keer/raadeens.py
+++ leerroutes/leren_programmeren/module3_keer_op_keer/raadeens.py
@@ -1,17 +1,17 @@
 import random
 
-score = 0
-for ronden in range(1,21):
-    num = random.randint(1,1000)
-    for geraden in range(10):
+score = 0                                 
+for ronden in range(1,21):        #20 ronden
+    num = random.randint(1,1000)  #random int
+    for geraden in range(10):     #je mag 10 keer raden
         raden = int(input("Raad het getal tussen 1 en 1000  "))
-        verschil = abs (num - raden)
+        verschil = abs (num - raden)  #absolute value of, als het in de min gaat word het weer positief
 
         if num > raden:
             print("Hoger!")
         if raden > num:
             print("Lager!")
-        elif raden == num:
+        elif raden == num:   #als het geraden is: break
             print("Geraden! Top!")
             score += 1
             break
@@ -23,19 +23,19 @@ for ronden in range(1,21):
             print("Je bent warm!")
 
     
-    if num != raden:
+    if num != raden:           #als je na 10 keer het nummer niet hebt geraden
         print(f"Je hebt het getal niet geraden! Het getal was {num}")
     if ronden == 20:
         print(f"Je hebt {ronden} rondes gespeeld! Je eindscore is {score}")
 
-    again = input(f"Je score is nu {score}. Wil je nog een keer?  ")
+    again = input(f"Je score is nu {score}. Wil je nog een keer?  ")      #wil je nog een keer
     if again in ("No", "n","Nee","nee"):
         print(f"Je hebt {ronden} rondes gespeeld! Je eindscore is {score}")
         print("Tot later!")
         quit()
 
 
-print(f"Je hebt {ronden} rondes gespeeld! Je eindscore is {score}")
+print(f"Je hebt {ronden} rondes gespeeld! Je eindscore is {score}")     
 
 
 
